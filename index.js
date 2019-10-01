32e5ed34e6df690fa85dcda2d11c4d83f2e34d21
190968 <19ham09@gmail.com>
2019-10-01 16:38:59 +0300

    my commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var path = require("path");
 var cors = require("cors");
 app.use(cors());
-
+const axios = require('axios');
 
 
 app.get('/', (req,res) => {
@@ -15,24 +15,20 @@ app.get('/', (req,res) => {
 
 
 app.get('/one',(req,res)=>{
-    console.log("Hello Fill");
-    res.send('hello Fill');
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(response => {   
+    res.send(response.data);
+})
+.catch(error => {
+  console.log(error);
 });
-app.get('/*',(req,res) => {
-     console.dir(req.path);
+    
+});
+app.get('/*',(req,res) => {    
      res.sendFile(path.join(__dirname,req.path))
 });
 
-// const server = http.createServer(function(req,res){
-//     fs.readFile('./index.html',function(err,data){
-//         if ( err ) throw err;
-        
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(data);
-//         res.end();
-//     })
-    
-// });
+
 const port = process.env.PORT || 3003;
 app.listen(port, (err)=>{
     if ( err ) { return  console.log("Error");
